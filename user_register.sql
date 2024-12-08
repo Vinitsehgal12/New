b78f730b101cd5e6930993edaf50a9684961f53b
Vinitsehgal12 <sehgalvinit70@gmail.com>
2024-12-08 12:10:23 +0530

    Add files via upload

---
+++ user_register.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 08, 2024 at 07:37 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `example_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_register`
+--
+
+CREATE TABLE `user_register` (
+  `id` int(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `dob` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `languages` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_register`
+--
+
+INSERT INTO `user_register` (`id`, `first_name`, `last_name`, `email`, `phone`, `dob`, `password`, `gender`, `languages`) VALUES
+(1, 'kamal', 'saini', 'kamal@gmail.com', '3333333333', '2004-05-05', 'K34444', 'male', 'Hindi,English,Punjabi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_register`
+--
+ALTER TABLE `user_register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_register`
+--
+ALTER TABLE `user_register`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
